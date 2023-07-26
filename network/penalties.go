cecee4fb8f068e1c454e6701757108a1357ffeb3
Joe Clapis <jclapis@outlook.com>
2023-07-26 02:23:54 -0400

    Added penalties and prices

--- network/penalties.go
+++ network/penalties.go
@@ -3,7 +3,6 @@ package network
 import (
 	"fmt"
 	"math/big"
-	"sync"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
@@ -11,33 +10,47 @@ import (
 	"github.com/rocket-pool/rocketpool-go/rocketpool"
 )
 
-// Estimate the gas of SubmitPenalty
-func EstimateSubmitPenaltyGas(rp *rocketpool.RocketPool, minipoolAddress common.Address, block *big.Int, opts *bind.TransactOpts) (rocketpool.GasInfo, error) {
-	rocketNetworkPenalties, err := getRocketNetworkPenalties(rp, nil)
-	if err != nil {
-		return rocketpool.GasInfo{}, err
-	}
-	return rocketNetworkPenalties.GetTransactionGasInfo(opts, "submitPenalty", minipoolAddress, block)
+const (
+	// Contract names
+	NetworkPenalties_ContractName string = "rocketNetworkPenalties"
+
+	// Transactions
+	networkPenalties_submitPenalty string = "submitPenalty"
+)
+
+// ===============
+// === Structs ===
+// ===============
+
+// Binding for RocketNetworkPenalties
+type NetworkPenalties struct {
+	rp       *rocketpool.RocketPool
+	contract *rocketpool.Contract
 }
 
-// Submit penalty for given minipool
-func SubmitPenalty(rp *rocketpool.RocketPool, minipoolAddress common.Address, block *big.Int, opts *bind.TransactOpts) (common.Hash, error) {
-	rocketNetworkPrices, err := getRocketNetworkPenalties(rp, nil)
+// ====================
+// === Constructors ===
+// ====================
+
+// Creates a new NetworkPenalties contract binding
+func NewNetworkPenalties(rp *rocketpool.RocketPool, opts *bind.CallOpts) (*NetworkPenalties, error) {
+	// Create the contract
+	contract, err := rp.GetContract(NetworkPenalties_ContractName, opts)
 	if err != nil {
-		return common.Hash{}, err
+		return nil, fmt.Errorf("error getting network penalties contract: %w", err)
 	}
-	tx, err := rocketNetworkPrices.Transact(opts, "submitPenalty", minipoolAddress, block)
-	if err != nil {
-		return common.Hash{}, fmt.Errorf("Could not submit penalty: %w", err)
-	}
-	return tx.Hash(), nil
+
+	return &NetworkPenalties{
+		rp:       rp,
+		contract: contract,
+	}, nil
 }
 
-// Get contracts
-var rocketNetworkPenaltiesLock sync.Mutex
+// ====================
+// === Transactions ===
+// ====================
 
-func getRocketNetworkPenalties(rp *rocketpool.RocketPool, opts *bind.CallOpts) (*rocketpool.Contract, error) {
-	rocketNetworkPenaltiesLock.Lock()
-	defer rocketNetworkPenaltiesLock.Unlock()
-	return rp.GetContract("rocketNetworkPenalties", opts)
+// Get info for minipool penalty submission
+func (c *NetworkPenalties) GetSubmitPenaltyInfo(minipoolAddress common.Address, block *big.Int, opts *bind.TransactOpts) (*rocketpool.TransactionInfo, error) {
+	return rocketpool.NewTransactionInfo(c.contract, networkPenalties_submitPenalty, opts, minipoolAddress, block)
 }
--- network/prices.go
+++ network/prices.go
@@ -3,80 +3,141 @@ package network
 import (
 	"fmt"
 	"math/big"
-	"sync"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/rocket-pool/rocketpool-go/rocketpool"
+	"github.com/rocket-pool/rocketpool-go/utils/eth"
+	"github.com/rocket-pool/rocketpool-go/utils/multicall"
 )
 
-// Get the block number which network prices are current for
-func GetPricesBlock(rp *rocketpool.RocketPool, opts *bind.CallOpts) (uint64, error) {
-	rocketNetworkPrices, err := getRocketNetworkPrices(rp, opts)
+const (
+	// Contract names
+	NetworkPrices_ContractName string = "rocketNetworkPrices"
+
+	// Calls
+	networkPrices_getPricesBlock           string = "getPricesBlock"
+	networkPrices_getRPLPrice              string = "getRPLPrice"
+	networkPrices_getLatestReportableBlock string = "getLatestReportableBlock"
+
+	// Transactions
+	networkPrices_submitPrices string = "submitPrices"
+)
+
+// ===============
+// === Structs ===
+// ===============
+
+// Binding for RocketNetworkPrices
+type NetworkPrices struct {
+	rp       *rocketpool.RocketPool
+	contract *rocketpool.Contract
+}
+
+// Multicall details for network prices
+type NetworkPricesDetails struct {
+	// Raw parameters
+	PricesBlockRaw                 *big.Int `json:"pricesBlockRaw"`
+	RplPriceRaw                    *big.Int `json:"rplPriceRaw"`
+	LatestReportablePricesBlockRaw *big.Int `json:"latestReportablePricesBlockRaw"`
+
+	// Formatted parameters
+	PricesBlock                 uint64  `json:"pricesBlock"`
+	RplPrice                    float64 `json:"rplPrice"`
+	LatestReportablePricesBlock uint64  `json:"latestReportablePricesBlock"`
+}
+
+// ====================
+// === Constructors ===
+// ====================
+
+// Creates a new NetworkPrices contract binding
+func NewNetworkPrices(rp *rocketpool.RocketPool, opts *bind.CallOpts) (*NetworkPrices, error) {
+	// Create the contract
+	contract, err := rp.GetContract(NetworkPrices_ContractName, opts)
 	if err != nil {
-		return 0, err
+		return nil, fmt.Errorf("error getting network prices contract: %w", err)
 	}
-	pricesBlock := new(*big.Int)
-	if err := rocketNetworkPrices.Call(opts, pricesBlock, "getPricesBlock"); err != nil {
-		return 0, fmt.Errorf("Could not get network prices block: %w", err)
-	}
-	return (*pricesBlock).Uint64(), nil
+
+	return &NetworkPrices{
+		rp:       rp,
+		contract: contract,
+	}, nil
+}
+
+// ===================
+// === Raw Getters ===
+// ===================
+
+// Get the block number which network prices are current for
+func (c *NetworkPrices) GetPricesBlockRaw(opts *bind.CallOpts) (*big.Int, error) {
+	return rocketpool.Call[*big.Int](c.contract, opts, networkPrices_getPricesBlock)
 }
 
 // Get the current network RPL price in ETH
-func GetRPLPrice(rp *rocketpool.RocketPool, opts *bind.CallOpts) (*big.Int, error) {
-	rocketNetworkPrices, err := getRocketNetworkPrices(rp, opts)
-	if err != nil {
-		return nil, err
-	}
-	rplPrice := new(*big.Int)
-	if err := rocketNetworkPrices.Call(opts, rplPrice, "getRPLPrice"); err != nil {
-		return nil, fmt.Errorf("Could not get network RPL price: %w", err)
-	}
-	return *rplPrice, nil
-}
-
-// Estimate the gas of SubmitPrices
-func EstimateSubmitPricesGas(rp *rocketpool.RocketPool, block uint64, rplPrice *big.Int, opts *bind.TransactOpts) (rocketpool.GasInfo, error) {
-	rocketNetworkPrices, err := getRocketNetworkPrices(rp, nil)
-	if err != nil {
-		return rocketpool.GasInfo{}, err
-	}
-	return rocketNetworkPrices.GetTransactionGasInfo(opts, "submitPrices", big.NewInt(int64(block)), rplPrice)
-}
-
-// Submit network prices and total effective RPL stake for an epoch
-func SubmitPrices(rp *rocketpool.RocketPool, block uint64, rplPrice *big.Int, opts *bind.TransactOpts) (common.Hash, error) {
-	rocketNetworkPrices, err := getRocketNetworkPrices(rp, nil)
-	if err != nil {
-		return common.Hash{}, err
-	}
-	tx, err := rocketNetworkPrices.Transact(opts, "submitPrices", big.NewInt(int64(block)), rplPrice)
-	if err != nil {
-		return common.Hash{}, fmt.Errorf("Could not submit network prices: %w", err)
-	}
-	return tx.Hash(), nil
+func (c *NetworkPrices) GetRplPriceRaw(opts *bind.CallOpts) (*big.Int, error) {
+	return rocketpool.Call[*big.Int](c.contract, opts, networkPrices_getRPLPrice)
 }
 
 // Returns the latest block number that oracles should be reporting prices for
-func GetLatestReportablePricesBlock(rp *rocketpool.RocketPool, opts *bind.CallOpts) (*big.Int, error) {
-	rocketNetworkPrices, err := getRocketNetworkPrices(rp, opts)
-	if err != nil {
-		return nil, err
-	}
-	latestReportableBlock := new(*big.Int)
-	if err := rocketNetworkPrices.Call(opts, latestReportableBlock, "getLatestReportableBlock"); err != nil {
-		return nil, fmt.Errorf("Could not get latest reportable block: %w", err)
-	}
-	return *latestReportableBlock, nil
+func (c *NetworkPrices) GetLatestReportablePricesBlockRaw(opts *bind.CallOpts) (*big.Int, error) {
+	return rocketpool.Call[*big.Int](c.contract, opts, networkPrices_getLatestReportableBlock)
 }
 
-// Get contracts
-var rocketNetworkPricesLock sync.Mutex
+// =========================
+// === Formatted Getters ===
+// =========================
 
-func getRocketNetworkPrices(rp *rocketpool.RocketPool, opts *bind.CallOpts) (*rocketpool.Contract, error) {
-	rocketNetworkPricesLock.Lock()
-	defer rocketNetworkPricesLock.Unlock()
-	return rp.GetContract("rocketNetworkPrices", opts)
+// Get the block number which network prices are current for
+func (c *NetworkPrices) GetPricesBlock(opts *bind.CallOpts) (uint64, error) {
+	raw, err := c.GetPricesBlockRaw(opts)
+	if err != nil {
+		return 0, err
+	}
+	return raw.Uint64(), nil
+}
+
+// Get the current network RPL price in ETH
+func (c *NetworkPrices) GetRplPrice(opts *bind.CallOpts) (float64, error) {
+	raw, err := c.GetRplPriceRaw(opts)
+	if err != nil {
+		return 0, err
+	}
+	return eth.WeiToEth(raw), nil
+}
+
+// Returns the latest block number that oracles should be reporting prices for
+func (c *NetworkPrices) GetLatestReportablePricesBlock(opts *bind.CallOpts) (uint64, error) {
+	raw, err := c.GetLatestReportablePricesBlockRaw(opts)
+	if err != nil {
+		return 0, err
+	}
+	return raw.Uint64(), nil
+}
+
+// ====================
+// === Transactions ===
+// ====================
+
+// Get info for network price submission
+func (c *NetworkPrices) GetSubmitPricesInfo(block uint64, rplPrice *big.Int, opts *bind.TransactOpts) (*rocketpool.TransactionInfo, error) {
+	return rocketpool.NewTransactionInfo(c.contract, networkPrices_submitPrices, opts, big.NewInt(int64(block)), rplPrice)
+}
+
+// =================
+// === Multicall ===
+// =================
+
+// Add queries to a multicall batcher
+func (c *NetworkPrices) AddMulticallQueries(mc *multicall.MultiCaller, details *NetworkPricesDetails) {
+	mc.AddCall(c.contract, &details.PricesBlockRaw, networkPrices_getPricesBlock)
+	mc.AddCall(c.contract, &details.RplPriceRaw, networkPrices_getRPLPrice)
+	mc.AddCall(c.contract, &details.LatestReportablePricesBlockRaw, networkPrices_getLatestReportableBlock)
+}
+
+// Postprocess the multicalled data to get the formatted parameters
+func (c *NetworkPrices) PostprocessAfterMulticall(details *NetworkPricesDetails) {
+	details.PricesBlock = details.PricesBlockRaw.Uint64()
+	details.RplPrice = eth.WeiToEth(details.RplPriceRaw)
+	details.LatestReportablePricesBlock = details.LatestReportablePricesBlockRaw.Uint64()
 }
